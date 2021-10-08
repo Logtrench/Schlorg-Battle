badb101db1adeb7f9a83cb576688aa60e01579c1
LOGAN MARSHY <91899169+Logtrench@users.noreply.github.com>
2021-10-08 20:05:38 +0000

    GUI is defaulted

--- GUI.java
+++ GUI.java
@@ -9,6 +9,8 @@ public class GUI implements ActionListener{
   private JPanel panel;
   private JLabel label;
   
+
+  //default method
   public GUI() 
   {
     //creating new objects of type frame and panel and button
@@ -49,6 +51,49 @@ public class GUI implements ActionListener{
 
 
   }
+
+  public GUI() 
+  {
+    //creating new objects of type frame and panel and button
+    frame = new JFrame();
+    JButton button = new JButton("Click me");
+    label = new JLabel("Number of clicks: " + count);
+    button.addActionListener(this);
+
+
+    JPanel panel = new JPanel();
+
+    //setting border of panel ()
+    panel.setBorder(BorderFactory.createEmptyBorder(100, 200, 50, 70));
+
+    //making layout, aka
+    panel.setLayout(new GridLayout(0, 1));
+
+
+    //add panel to frame
+    frame.add(panel, BorderLayout.CENTER);
+
+    //add button
+    panel.add(button);
+    //add  label
+    panel.add(label);
+
+    //set closing behaviour
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //title
+    frame.setTitle("Gui");
+
+    //set window to match certain size
+    frame.pack();
+
+    //make it visible.
+    frame.setVisible(true);
+
+
+  }
+
+
   
   @Override
   public void actionPerformed(ActionEvent e)
