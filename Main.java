1a9b7423f203f0872fb07701f87d5b953ab0134a
LOGAN MARSHY <91899169+Logtrench@users.noreply.github.com>
2021-10-12 17:13:01 +0000

    Added colo(u)r possibility and grid layout

--- Main.java
+++ Main.java
@@ -2,76 +2,65 @@ import javax.swing.*;
 import java.awt.*;
 
 class Main {
-  public static void main(String[] args) 
-  {
-    if(again())
-    {
+  public static void main(String[] args) {
+    if (again()) {
       play();
-    }else{
+    } else {
       exit();
     }
   }
 
-  public static boolean again()
-  {
+  public static boolean again() {
 
-    
     return true;
-
 
   }
 
-  public static void play()
-  {
-    
-    
+  public static void play() {
+
+    GridLayout OneByOne = new GridLayout(0, 1, 3, 10);
+
     JPanel panel = new JPanel();
     ImageIcon icon = new ImageIcon("images/bear.png");
 
-    panel = GUI.panel(500, 500);
-    GUI.labl(panel, "Battle for Schlorg", 50, 10, 25, 50);
+    panel = GUI.panel(250, 250);
+    panel.setLayout(OneByOne);
+    panel.setBackground(Color.green);
+
+    GUI.labl(panel, "Battle for Schlorg", 10, 25, 50, 10);
 
     JButton join = new JButton();
-    join = GUI.butt(panel, "join", 100, 100, 100, 100);
+    join = GUI.butt(panel, "join", 25, 25, 25, 25);
+    join.setBackground(Color.YELLOW);
+    join.setForeground(Color.BLACK);
     join.addActionListener(new GUI());
     JButton exit = new JButton();
-    exit = GUI.butt(panel, "exit", 100, 100, 100, 100);
+    exit = GUI.butt(panel, "exit", 25, 25, 25, 25);
     exit.addActionListener(new GUI());
-    
-        //image(panel, icon, 10, 120, 250, 250);
-    //GUI.image(panel, icon, 100, 120, 250, 250);
-      
-      
-      String[] ba = {"1","2"};
-      //Inputs.main(ba);
 
-      
-      System.out.println("yayy");
+    // image(panel, icon, 10, 120, 250, 250);
+    // GUI.image(panel, icon, 100, 120, 250, 250);
+
+    String[] ba = { "1", "2" };
+    // Inputs.main(ba);
+
+    System.out.println("yayy");
   }
 
-  public static void exit()
-  {
-      System.out.println("byeee mofo");
+  public static void exit() {
+    System.out.println("byeee mofo");
   }
-  
-  public static String input(String question)
-  {
-    
+
+  public static String input(String question) {
 
     return "baba";
   }
 
 }
 
-
-
-
 /*
-manifesto
-sitrng bitte = please / your're welcome
-scheisse du mensch du
-dankeschön = thankyou very much
-bitteschön = bitte very much
-moutarde = mustard indeed
-
-*/
+ * manifesto sitrng bitte = please / your're welcome scheisse du mensch du
+ * dankeschön = thankyou very much bitteschön = bitte very much moutarde =
+ * mustard indeed
+ * 
+ */
